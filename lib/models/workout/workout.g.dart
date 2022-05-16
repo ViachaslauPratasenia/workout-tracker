b013181ae3416d662b504845ce01da0a996e1d5f
vpratasenia <v.pratasenia@aisnovations.com>
2022-05-16 18:36:37 +0400

    added workout hive adapter

---
+++ lib/models/workout/workout.g.dart
@@ -0,0 +1,76 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'workout.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class WorkoutAdapter extends TypeAdapter<Workout> {
+  @override
+  final int typeId = 3;
+
+  @override
+  Workout read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Workout(
+      id: fields[0] as int?,
+      title: fields[1] as String,
+      days: (fields[2] as List).cast<Day>(),
+      description: fields[3] as String?,
+      level: fields[4] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Workout obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.days)
+      ..writeByte(3)
+      ..write(obj.description)
+      ..writeByte(4)
+      ..write(obj.level);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is WorkoutAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Workout _$WorkoutFromJson(Map<String, dynamic> json) => Workout(
+      id: json['id'] as int?,
+      title: json['title'] as String,
+      days: (json['days'] as List<dynamic>?)
+              ?.map((e) => Day.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const [],
+      description: json['description'] as String?,
+      level: json['level'] as int? ?? 0,
+    );
+
+Map<String, dynamic> _$WorkoutToJson(Workout instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'days': instance.days,
+      'description': instance.description,
+      'level': instance.level,
+    };
